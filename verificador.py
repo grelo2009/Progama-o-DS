40748e26669c075dc984315eea5f15850d1cfa9a
grelo2009 <angelofaria2009@gmail.com>
2026-03-20 07:59:47 -0300

    Create verificador.py

---
+++ verificador.py
@@ -0,0 +1,7 @@
+# Verifica se o número é par
+if numero % 2 == 0:
+    # Se for par, imprime a mensagem
+    print(f"{numero} é par.")
+else:
+    # Se for ímpar, imprime a mensagem
+    print(f"{numero} é ______________.")
